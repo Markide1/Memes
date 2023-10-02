2283e76cb7a4e190649e22d62bb5fe64b2db641e
Markide1 <markndwiga@gmail.com>
2023-10-02 20:02:29 +0300

    Our first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This repo contains filesand directories forthe Lincon memes KE
---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, intial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE-edge">
+    <title>Lincoln_KE Memes Page</title>
+    <link rel="stylesheet" href="styles.css">
+  </head>
+  <body>
+    <h2>Welcome to Memes Arena: Where a laughter a day, keeps depression away!</h2>
+    <p>Do you want to upload a meme? Then click on "Choose file" choose your meme and click Upload!</p>
+    <form action="upload.php" method="post" enctype="multipart/form-data">
+		<label for="file"></label>
+		<input type="file" name="file" id="file">
+		<input type="submit" name="submit" value="Upload">
+	</form>
+    <p>Want to view memes instead? Click More Memes to check the latest!</p>
+    <input type="submit" value="More Memes" /> <br>
+    <p>Now check out my socials for more:</p>
+    <a href="https://wa.me/254741847620" target="_blank"><button>Whatsapp</button></a>&nbsp;&nbsp;&nbsp;
+    <a href="https://x.com/I_am_Lincoln_KE?t=NWWrxE-B8iEwW-7Qa7LLJw&s=09" target="_blank"><button>X</button></a>&nbsp;&nbsp;&nbsp;
+    <a href="https://t.me/lincoln_KEmemes" target="_blank"><button>Telegram</button></a>&nbsp;&nbsp;&nbsp;
+    <a href="https://instagram.com/i_am_lincoln_ke?igshid=MzRlODBiNWFlZA==" target="_blank"><button>Instagram</button></a>&nbsp;&nbsp;&nbsp;
+    <a href="https://www.youtube.com/@lincolnketv9697" target="_blank"><button>Youtube</button></a><br>
+    <h4>Use the socials to Contact Us and advertise on this page</h4><br>
+   
+<!-----
+   
+    <div class="copyright">
+      <p>©Lincoln_KE Memes:The home of laughter!</p>
+    </div>
+  ------>
+
+  </body> 
+</html>
